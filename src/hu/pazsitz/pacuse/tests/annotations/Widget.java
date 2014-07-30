606c239cd862142e37d285f086725bd0d6e3e4df
Zoltan Pazsit <pazsitz@pazsitz.hu>
2014-07-31 00:15:38 +0200

    create Widget annotation to detect widgets in page models

---
+++ src/hu/pazsitz/pacuse/tests/annotations/Widget.java
@@ -0,0 +1,18 @@
+package hu.pazsitz.pacuse.tests.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * DataTableAttributes.java
+ *
+ * @author Zoltan Pazsit <pazsitz@pazsitz.hu>
+ * @copyright Copyright (c) 2014, Zoltan Pazsit
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Widget {
+
+}
