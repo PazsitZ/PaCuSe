ac6e7700c435d61fd2d8ea7995965d699095b37b
Zoltan Pazsit <pazsitz@pazsitz.hu>
2014-09-21 12:56:05 +0200

    implement abstract method in GeneralWidgetImpl

--- src/hu/pazsitz/pacuse/pages/GeneralWidgetImpl.java
+++ src/hu/pazsitz/pacuse/pages/GeneralWidgetImpl.java
@@ -1,6 +1,8 @@
 package hu.pazsitz.pacuse.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 /**
  * GeneralWidgetImpl.java
@@ -10,8 +12,21 @@ import org.openqa.selenium.WebDriver;
  */
 public class GeneralWidgetImpl extends AbstractWidget {
 
-	public GeneralWidgetImpl(WebDriver webDriver) {
+	private WebElement container;
+	
+	public GeneralWidgetImpl(WebDriver webDriver, WebElement element) {
 		super(webDriver);
+		this.container = element;
+	}
+	
+	public GeneralWidgetImpl(WebDriver webDriver, By by) {
+		super(webDriver);
+		this.container = getWebDriver().findElement(by);
+	}
+
+	@Override
+	public WebElement getContainer() {
+		return container;
 	}
 
 }
